c83e340385761d146374b2358763b02ba604cbd7
yhao21 <13701304462@163.com>
2021-05-04 10:17:27 -0400

    i3wm

--- notes_for_ubuntu/some_backup/manjaro_backup/i3/initialize_commands.sh
+++ notes_for_ubuntu/some_backup/manjaro_backup/i3/initialize_commands.sh
@@ -3,4 +3,4 @@
 
 xinput set-prop "USB OPTICAL MOUSE" "Coordinate Transformation Matrix" 0.2 0 0 0 0.2 0 0 0 1
 xinput --disable "SynPS/2 Synaptics TouchPad"
-xmodmap ~/.xmodmap
+#xmodmap ~/.xmodmap
