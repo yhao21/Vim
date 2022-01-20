7aeba833e211e7374029bad3c2d6372d558e6f5c
yhao21 <13701304462@163.com>
2022-01-20 02:22:51 -0500

    i3wm nvidia

--- notes_for_ubuntu/some_backup/popOS_laptop/i3/initialize_commands.sh
+++ notes_for_ubuntu/some_backup/popOS_laptop/i3/initialize_commands.sh
@@ -4,3 +4,7 @@
 xinput set-prop "Logitech USB Optical Mouse" "Coordinate Transformation Matrix" 0.2 0 0 0 0.2 0 0 0 1
 xinput --disable "ELAN0412:00 04F3:3162 Touchpad"
 #xmodmap ~/.xmodmap
+
+
+
+xset -dpms s off
